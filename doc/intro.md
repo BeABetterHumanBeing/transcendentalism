ebf34ac5b7ed97cbf5cfcc13ac21a1e436bd218d
Daniel Gierl <daniel.erik.gierl@gmail.com>
2020-01-24 16:23:05 -0800

    adding boilerplate leiningen files

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to transcendentalism
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/transcendentalism/core_test.clj
@@ -0,0 +1,7 @@
+(ns transcendentalism.core-test
+  (:require [clojure.test :refer :all]
+            [transcendentalism.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
