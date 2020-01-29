a5f4a23635e599c3eb78ffb1fe6abb8695786a63
Daniel Gierl <daniel.erik.gierl@gmail.com>
2020-01-28 17:15:26 -0800

    Adding stylesheet to output, adding debug class

--- src/transcendentalism/generate.clj
+++ src/transcendentalism/generate.clj
@@ -1,6 +1,10 @@
 (ns transcendentalism.generate
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+    [clojure.string :as str]))
 (use 'transcendentalism.graph)
+
+; Whether to generate debugging HTML or not.
+(def debug true)
 
 (defn- gen-key
   "Generates a random alphanumeric key of given length"
@@ -29,9 +33,37 @@
   (doseq [file (.listFiles (io/as-file dirname))]
     (io/delete-file file)))
 
+(defn- attr-aware
+  "Adds attributes to a given opening tag."
+  [tagname attrs]
+  (if (empty? attrs)
+    (str "<" tagname ">")
+    (let [formatted-attrs (map #(str (first %) "=\"" (second %) "\"") (seq attrs))]
+      (str "<" tagname " " (str/join " " formatted-attrs) ">"))))
+
 (defn- html
+  [& contents]
+  (str "<html>" (apply str contents) "</html>"))
+
+(defn- head
   [contents]
-  (str "<html>" contents "</html>"))
+  (str "<head>" contents "</head>"))
+
+(defn- link
+  [rel href]
+  (str "<link rel=\"" rel "\" href=\"" href "\">"))
+
+(defn- body
+  [& contents]
+  (str "<body>" (apply str contents) "</body>"))
+
+(defn- div
+  [attrs & contents]
+  (str (attr-aware "div" attrs) (apply str contents) "</div>"))
+
+(defn- p
+  [contents]
+  (str "<p>" contents "</p>"))
 
 (defn- ul
   [contents]
@@ -43,10 +75,26 @@
     (map (fn [content] (str "<li>" content "</li>"))
       contents)))
 
+(defn- stylesheet
+  []
+  (str "div.debug {"
+    "font-family: Monaco, monospace;"
+    "border-style: dashed;"
+    "border-width: 1px;"
+    "border-color: red;"
+    "padding: 5px 10px 5px;"
+    (if debug "" "visibility:hidden")
+    "}"))
+
 (defn- generate-essay-segment
   "Returns the HTML corresponding to a given essay segment"
   [graph encodings sub]
-  (html (ul (li (map print-triple (all-triples graph sub))))))
+  (html
+    (head (link "stylesheet" "styles.css"))
+    (body
+      (if debug
+        (div {"class" "debug"} (p "Triples") (ul (li (map print-triple (all-triples graph sub)))))
+        ""))))
 
 (defn generate-output
   "Convert a validated graph into the HTML, CSS, and JS files that compose the website"
@@ -59,4 +107,5 @@
         [filename (str "output/" (sub encodings) ".html")]
         (do
           (spit filename (generate-essay-segment graph encodings sub))
-          (println "Generated" filename))))))
+          (println "Generated" filename))))
+    (spit "output/styles.css" (stylesheet))))
