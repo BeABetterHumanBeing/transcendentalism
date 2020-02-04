4764c2888ac17b0a3d4a52ee24c93cc331e6fe76
Daniel Gierl <daniel.erik.gierl@gmail.com>
2020-02-04 10:00:56 -0800

    Add some experimental internal nodes to monad svg, to play with time-based radius

--- src/transcendentalism/svg.clj
+++ src/transcendentalism/svg.clj
@@ -1,5 +1,7 @@
 (ns transcendentalism.svg
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [java-time :as jt]))
+
 ; TODO(gierl): generate is only called for some of the XML-related convenience
 ; functions. Move these into their own file.
 (use 'transcendentalism.generate)
@@ -15,6 +17,12 @@
         factor (* Math/PI 2)] ; To get to PI-based system
     [(+ (/ width 2) (* r (Math/cos (* t factor))))
      (+ (/ height 2) (* r (Math/sin (* t factor))))]))
+
+(defn- t-to-r
+  "Converts a timestamp into a radius"
+  [k max-r t]
+  (let [seconds-before-present (jt/as (jt/duration (jt/instant) t) :seconds)]
+    (* max-r (Math/pow k seconds-before-present))))
 
 (defn- svg
   "Returns the XML for an SVG"
@@ -72,6 +80,22 @@
           (seq (zipmap
             ["#f6bb05" "#f81308" "#3c35ff" "#08caf8" "#c853ff" "#29db01" "#f58705"]
             (range 7)))))
+      ; A bunch of evenly-spaced temporal circles
+      (str/join "\n"
+        (map (fn [timestamp]
+          (let [max-r (* width 0.45),
+                r (t-to-r 1.2 max-r timestamp),
+                coords (polar-to-cartesian width height r 0),
+                radius (+ (* 12 (/ r max-r)) 1)]
+            (circle {
+              "cx" (first coords),
+              "cy" (second coords),
+              "r" radius,
+              "fill" "white",
+              "stroke" "black",
+              "stroke-width" (/ radius 3),
+            })))
+        (take 10 (jt/iterate jt/minus (jt/minus (jt/instant) (jt/seconds 1)) (jt/seconds 1)))))
       )))
 
 (defn svg-to-image
