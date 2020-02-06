2f60dc79131aa08c1587520563c2ea2f4eab73fb
BeABetterHumanBeing <daniel.erik.gierl@gmail.com>
2020-02-06 12:45:59 -0800

    Adding stubs for four more essay segments to round out the meta/introductory aspects of the site

--- src/transcendentalism/core.clj
+++ src/transcendentalism/core.clj
@@ -24,11 +24,45 @@
         "It is the godhead, the point from which all things originate,"
         "and the point to which all things return."]))
     (->Triple :monad-intro-quote "/item/quote/author" "Daniel Gierl")
-    ; TODO - Add /essay/flow/next to the full introduction.
+    (->Triple :monad "/essay/flow/next" :welcome)
     ; TODO - Add /essay/flow/see_also to the top-level menu of metaphysics essays.
    ]))
 
-(def graph (construct-graph (concat monad)))
+; About
+; This sequence of essay_segments say a little about myself, this website, and
+; what I hope to do with it.
+(def about
+  (flatten [
+    ; Welcome
+    (types :welcome "/essay_segment")
+    (->Triple :welcome "/essay/contains" :welcome-contents)
+    (->Triple :welcome "/essay/title" "Welcome")
+    (types :welcome-contents "/item/ordered_set")
+    ; TODO(gierl) apologize for shitty website, explain purpose of site
+
+    ; I Am Dan
+    (types :i-am-dan "/essay_segment")
+    (->Triple :i-am-dan "/essay/contains" :i-am-dan-contents)
+    (->Triple :i-am-dan "/essay/title" "I Am Dan")
+    (types :i-am-dan-contents "/item/ordered_set")
+    ; TODO(gierl) brief history
+
+    ; Connections
+    (types :connections "/essay_segment")
+    (->Triple :connections "/essay/contains" :connections-contents)
+    (->Triple :connections "/essay/title" "Connections")
+    (types :connections-contents "/item/ordered_set")
+    ; TODO(gierl) contact information, respective responsibilities
+
+    ; Apologies
+    (types :apologies "/essay_segment")
+    (->Triple :apologies "/essay/contains" :apologies-contents)
+    (->Triple :apologies "/essay/title" "Apologies")
+    (types :apologies-contents "/item/ordered_set")
+    ; TODO(gierl) apologize
+  ]))
+
+(def graph (construct-graph (concat monad about)))
 
 (defn -main
   "I don't do a whole lot."
