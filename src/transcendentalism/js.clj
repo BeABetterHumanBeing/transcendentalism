2e6d0452e9b8263bad6af59a2afd35b157f81fbc
BeABetterHumanBeing <daniel.erik.gierl@gmail.com>
2020-02-07 16:04:36 -0800

    Refactoring JS function calls to a more clojure-friendly form

--- src/transcendentalism/js.clj
+++ src/transcendentalism/js.clj
@@ -23,75 +23,66 @@
 
 (defn js-array [values] (str "[" (str/join "," values) "]"))
 
+(defn- js-fn [name args & contents]
+  (str/join "\n"
+    [(str "function " name "(" (str/join ", " args) ") {")
+     (str/join "\n" contents)
+     "}"]))
+
 (defn- loadwith
   "Function for replacing a div with the results of a load call"
   []
-  (str/join "\n" [
-    "function loadWith(elem, url, callback) {"
-      ; Courtesy Victor 'Chris' Cabral
-      "$.get(url,function(d){"
-        "elem.replaceWith(d);"
-        "callback();"
-      "});"
-    "}"
-  ]))
+  (js-fn "loadWith" ["elem" "url" "callback"]
+    ; Courtesy Victor 'Chris' Cabral
+    "$.get(url,function(d){"
+      "elem.replaceWith(d);"
+      "callback();"
+    "});"))
 
 (defn- maybe-insert-divider
   "Inserts a divider between two segments if they are non-adjacent"
   []
-  (str/join "\n" [
-    "function maybeInsertDivider(a, b) {"
-      "if(!($('#' + a + '-' + b).length)) {"
-        (str "$('<div class=\"ellipsis\"></div>').insertAfter(" (js-seg-id "a" "footer") ");")
-      "}"
-    "}"
-  ]))
+  (js-fn "maybeInsertDivider" ["a" "b"]
+    "if(!($('#' + a + '-' + b).length)) {"
+      (str "$('<div class=\"ellipsis\"></div>').insertAfter(" (js-seg-id "a" "footer") ");")
+    "}"))
 
 (defn- center-view-on
   "Moves the window to center the view on the start of a given segment"
   []
-  (str/join "\n" [
+  (js-fn "centerViewOn" ["encoded_id"]
     ; TODO(gierl): Change URL to new segment, caching old one in history.
-    "function centerViewOn(encoded_id) {"
-      (str "$(" (js-seg-id "encoded_id" "") ").get(0).scrollIntoView({behavior: 'smooth'});")
-    "};"
-  ]))
+    (str "$(" (js-seg-id "encoded_id" "") ").get(0).scrollIntoView({behavior: 'smooth'});")))
 
 (defn- segment-loaded-callback
   "Function that is called when a segment's body is loaded"
   []
-  (str/join "\n" [
-    "function segmentLoadedCallback(origin, encoded_id,homes) {"
-      "if (homes.length > 0) {"
-        (str "loadWith($(" (js-seg-id "encoded_id" "above") "), homes[0] + '.html', function() {")
-          "maybeInsertDivider(homes[0], encoded_id);"
-          "segmentLoadedCallback(origin, homes[0], homes.slice(1, homes.length));"
-        "});"
-      "} else {"
-        "centerViewOn(origin);"
-      "}"
-    "}"
-  ]))
+  (js-fn "segmentLoadedCallback" ["origin" "encoded_id" "homes"]
+    "if (homes.length > 0) {"
+      (str "loadWith($(" (js-seg-id "encoded_id" "above") "), homes[0] + '.html', function() {")
+        "maybeInsertDivider(homes[0], encoded_id);"
+        "segmentLoadedCallback(origin, homes[0], homes.slice(1, homes.length));"
+      "});"
+    "} else {"
+      "centerViewOn(origin);"
+    "}"))
 
 (defn- openSegment
   "Function that is called when an internal link is clicked"
   []
-  (str/join "\n" [
-    "function openSegment(encoded_from,encoded_to) {"
-      (debug "console.log('Opening ' + encoded_to + ' from ' + encoded_from);")
-      (str "if ($(" (js-seg-id "encoded_to" "") ").length) {")
+  (js-fn "openSegment" ["encoded_from" "encoded_to"]
+    (debug "console.log('Opening ' + encoded_to + ' from ' + encoded_from);")
+    (str "if ($(" (js-seg-id "encoded_to" "") ").length) {")
+      "centerViewOn(encoded_to);"
+    "} else {"
+      ; TODO(gierl): Clear all segments beneath encoded_from.
+      (str "$('<div id=\"insertion-pt\"></div>').insertAfter($(" (js-seg-id "encoded_from" "footer") "));")
+      "loadWith($('#insertion-pt'), encoded_to + '.html', function() {"
+        (str "$(" (js-seg-id "encoded_to" "above") ").remove();")
+        (str "$(" (js-seg-id "encoded_from" "buffer") ").remove();")
         "centerViewOn(encoded_to);"
-      "} else {"
-        ; TODO(gierl): Clear all segments beneath encoded_from.
-        (str "$('<div id=\"insertion-pt\"></div>').insertAfter($(" (js-seg-id "encoded_from" "footer") "));")
-        "loadWith($('#insertion-pt'), encoded_to + '.html', function() {"
-          (str "$(" (js-seg-id "encoded_to" "above") ").remove();")
-          (str "$(" (js-seg-id "encoded_from" "buffer") ").remove();")
-          "centerViewOn(encoded_to);"
-        "});"
-      "}"
-    "}"
-  ]))
+      "});"
+    "}"))
 
 (defn script
   "Return the JavaScript for the website"
