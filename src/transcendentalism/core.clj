49ac02c99bccf5e0a3bb8e928b0488aed52652b8
BeABetterHumanBeing <daniel.erik.gierl@gmail.com>
2020-02-15 22:54:14 -0800

    Adding back big emoji support

--- src/transcendentalism/core.clj
+++ src/transcendentalism/core.clj
@@ -136,6 +136,14 @@
          []
          (->Triple item-keyword "/item/quote/author" author))])))
 
+(defn- big-emoji-segment
+  [sub emoji]
+  (let [item-keyword (keyword (str (name sub) "-i"))]
+    [(types schema sub "/segment")
+     (->Triple sub "/segment/contains" item-keyword)
+     (types schema item-keyword "/item/big_emoji")
+     (->Triple item-keyword "/item/big_emoji/emoji" emoji)]))
+
 ; The monad
 ; This essay_segment serves as the default entry point into the graph.
 (def monad
@@ -163,98 +171,100 @@
 ; About
 ; This sequence of essay_segments say a little about myself, this website, and
 ; what I hope to do with it.
-; (def about
-;   (flatten [
-;     (essay-series [:monad :welcome :i-am-dan :connections :apologies])
-;     (directive-under-construction :connections :apologies)
+(def about
+  (flatten [
+    (essay-series [:monad :welcome ;:i-am-dan :connections :apologies
+    ])
+    ; (directive-under-construction :connections :apologies)
 
-;     ; Welcome
-;     (essay :welcome "Welcome" [
-;       :wave-emoji :welcome-1 :welcome-2 :footnote-1 :welcome-3 :footnote-2
-;       :welcome-4 :praying-hands-emoji])
-;     (types schema :wave-emoji "/item/big_emoji")
-;     (->Triple :wave-emoji "/item/big_emoji/emoji" "&#x1f44b")
-;     (text :welcome-1
-;       "Hi there! I'm Daniel Gierl, and I'd like to welcome you to my personal"
-;       "website, Transcendental Metaphysics! I use this space to explore"
-;       "questions of philosophy, religion, politics, you name it. It is my"
-;       "sincere hope that you leave feeling enriched by the experience, and that"
-;       "the time you spend here is time well spent.")
-;     (text :welcome-2
-;       "I apologize in advance for any issues you may encounter with the"
-;       "unorthodox structure of the site; I've been using it as a playground"
-;       "for some of the more experimental ideas I've been toying with. I wrote the"
-;       "whole thing"
-;       (tangent :footnote-1 "from scratch")
-;       "and, as a backend engineer, this was a recipe for, ummm, how shall we"
-;       "say, *curious* frontend design choices.")
-;     (footnote :welcome :footnote-1
-;       "In clojure, no less. I used it as an opportunity to teach myself"
-;       "the language. There is no learning quite like doing.")
-;     (text :welcome-3
-;       "The whole site is structured as a big, tangled graph. What you're"
-;       "reading here is as close to a proper 'beginning' as it gets, and there"
-;       "is nothing out there that resembles an 'end'. My intention is that"
-;       "wandering through these pages will be an experience not unlike wandering"
-;       "through a garden maze; getting lost is half the fun, and there is all"
-;       "kinds of"
-;       (tangent :footnote-2 "treasure")
-;       "hidden away for you to find.")
-;     (footnote :welcome :footnote-2
-;       "As an aside, I have tried to make the URLs somewhat stable so that they"
-;       "can be shared and saved, but I can only guarantee a modicum of stability"
-;       "in a shifting sea of ideas.")
-;     (poem :welcome-4
-;       "May you find that which you search for"
-;       "May your bridges meet you halfway"
-;       "May you never lose yourself in darkness"
-;       "And may the light of God shine brightly on your soul")
-;     (types schema :praying-hands-emoji "/item/big_emoji")
-;     (->Triple :praying-hands-emoji "/item/big_emoji/emoji" "&#x1f64f")
+    ; Welcome
+    (essay :welcome "Welcome" :wave-emoji)
+    (big-emoji-segment :wave-emoji "&#x1f44b")
 
-;     ; I Am Dan
-;     (essay :i-am-dan "I Am Dan" [
-;       :yellow-socks :salient-details :prefer-socks :not-privacy])
-;     (text :yellow-socks
-;       "My name's Daniel Gierl. I was a plump, healthy baby with a full head of"
-;       "hair, and I went home from the hospital wearing little yellow socks.")
-;     ; TODO yellow socks (preferably a photo)
-;     (text :salient-details
-;       "In life, we often overlook the smaller, sentimental details. A person is"
-;       "readily reduced to their age and occupation, their parentage, their"
-;       "claim to fame... There's nothing wrong with this; it happens for a"
-;       "reason. If you're only going to remember one thing about a person, it"
-;       "makes sense to remember just their most salient trait. Sometimes, the"
-;       "salient trait isn't even what they would think is noteworthy: it's not"
-;       "uncommon in the annals of history to find a person whose existence is"
-;       "remembered solely because of the consequences of some rash action or"
-;       "small act of kindness. The passions, trials, and triumphs that they"
-;       "occupied their lives with are forgotten on account of being a bit too"
-;       "mundane, a bit too undocumented.")
-;     (text :prefer-socks
-;       "I do have a diary, but it's not on this website. If you read enough of"
-;       "this, you will surely get a great sense of who I am, but it's not"
-;       "*really* supposed to be about me. Given this opportunity to toot my own"
-;       "horn, I'd much rather be known as \"the guy who went home from the"
-;       "hospital in yellow socks\" than anything else.")
-;     (text :not-privacy
-;       "It's not even about privacy. I tell a lot of stories, so you will surely"
-;       "get plenty of opportunities to get to know me. You'll just have to dig"
-;       "around a little to find them.")
+    ;:welcome-1 :welcome-2 :footnote-1 :welcome-3 :footnote-2
+      ;:welcome-4 :praying-hands-emoji
 
-;     ; Connections
-;     (essay :connections "Connections" [])
-;     ; TODO(gierl) contact information, respective responsibilities
+    ; (text-segment :welcome-1
+    ;   "Hi there! I'm Daniel Gierl, and I'd like to welcome you to my personal"
+    ;   "website, Transcendental Metaphysics! I use this space to explore"
+    ;   "questions of philosophy, religion, politics, you name it. It is my"
+    ;   "sincere hope that you leave feeling enriched by the experience, and that"
+    ;   "the time you spend here is time well spent.")
+    ; (text :welcome-2
+    ;   "I apologize in advance for any issues you may encounter with the"
+    ;   "unorthodox structure of the site; I've been using it as a playground"
+    ;   "for some of the more experimental ideas I've been toying with. I wrote the"
+    ;   "whole thing"
+    ;   (tangent :footnote-1 "from scratch")
+    ;   "and, as a backend engineer, this was a recipe for, ummm, how shall we"
+    ;   "say, *curious* frontend design choices.")
+    ; (footnote :welcome :footnote-1
+    ;   "In clojure, no less. I used it as an opportunity to teach myself"
+    ;   "the language. There is no learning quite like doing.")
+    ; (text :welcome-3
+    ;   "The whole site is structured as a big, tangled graph. What you're"
+    ;   "reading here is as close to a proper 'beginning' as it gets, and there"
+    ;   "is nothing out there that resembles an 'end'. My intention is that"
+    ;   "wandering through these pages will be an experience not unlike wandering"
+    ;   "through a garden maze; getting lost is half the fun, and there is all"
+    ;   "kinds of"
+    ;   (tangent :footnote-2 "treasure")
+    ;   "hidden away for you to find.")
+    ; (footnote :welcome :footnote-2
+    ;   "As an aside, I have tried to make the URLs somewhat stable so that they"
+    ;   "can be shared and saved, but I can only guarantee a modicum of stability"
+    ;   "in a shifting sea of ideas.")
+    ; (poem :welcome-4
+    ;   "May you find that which you search for"
+    ;   "May your bridges meet you halfway"
+    ;   "May you never lose yourself in darkness"
+    ;   "And may the light of God shine brightly on your soul")
+    ; (types schema :praying-hands-emoji "/item/big_emoji")
+    ; (->Triple :praying-hands-emoji "/item/big_emoji/emoji" "&#x1f64f")
 
-;     ; Apologies
-;     (essay :apologies "Apologies" [])
-;     ; TODO(gierl) apologize
-;   ]))
+    ; I Am Dan
+    ; (essay :i-am-dan "I Am Dan" [
+    ;   :yellow-socks :salient-details :prefer-socks :not-privacy])
+    ; (text :yellow-socks
+    ;   "My name's Daniel Gierl. I was a plump, healthy baby with a full head of"
+    ;   "hair, and I went home from the hospital wearing little yellow socks.")
+    ; ; TODO yellow socks (preferably a photo)
+    ; (text :salient-details
+    ;   "In life, we often overlook the smaller, sentimental details. A person is"
+    ;   "readily reduced to their age and occupation, their parentage, their"
+    ;   "claim to fame... There's nothing wrong with this; it happens for a"
+    ;   "reason. If you're only going to remember one thing about a person, it"
+    ;   "makes sense to remember just their most salient trait. Sometimes, the"
+    ;   "salient trait isn't even what they would think is noteworthy: it's not"
+    ;   "uncommon in the annals of history to find a person whose existence is"
+    ;   "remembered solely because of the consequences of some rash action or"
+    ;   "small act of kindness. The passions, trials, and triumphs that they"
+    ;   "occupied their lives with are forgotten on account of being a bit too"
+    ;   "mundane, a bit too undocumented.")
+    ; (text :prefer-socks
+    ;   "I do have a diary, but it's not on this website. If you read enough of"
+    ;   "this, you will surely get a great sense of who I am, but it's not"
+    ;   "*really* supposed to be about me. Given this opportunity to toot my own"
+    ;   "horn, I'd much rather be known as \"the guy who went home from the"
+    ;   "hospital in yellow socks\" than anything else.")
+    ; (text :not-privacy
+    ;   "It's not even about privacy. I tell a lot of stories, so you will surely"
+    ;   "get plenty of opportunities to get to know me. You'll just have to dig"
+    ;   "around a little to find them.")
 
-(def graph (construct-graph (apply-directives
-  monad
-  ; about
-  )))
+    ; Connections
+    ; (essay :connections "Connections" [])
+    ; TODO(gierl) contact information, respective responsibilities
+
+    ; Apologies
+    ; (essay :apologies "Apologies" [])
+    ; TODO(gierl) apologize
+  ]))
+
+(def graph
+  (construct-graph
+    (apply-directives
+      monad about)))
 
 (defn -main
   "Validates the website's graph, and generates its files"
