c90839a4c79c9146a1c7ebe4c132739a514c698d
BeABetterHumanBeing <daniel.erik.gierl@gmail.com>
2020-02-17 15:25:10 -0800

    Uncommenting as much as I dare without breaking the new flow

--- src/transcendentalism/core.clj
+++ src/transcendentalism/core.clj
@@ -124,16 +124,17 @@
     (apply text footnote-sub fragments)
     (->Triple sub "/item/footnote" footnote-sub)))
 
-(defn- poem
-  "Creates an /item/poem"
+(defn- poem-segment
   [sub & lines]
-  (concat
-    (types schema sub "/item/poem")
-    (map
-      (fn [i]
-        (let [line (nth lines i)]
-          (->Triple sub "/item/poem/line" ^{:order i} [line])))
-      (range (count lines)))))
+  (let [item-keyword (keyword (str (name sub) "-i"))]
+    [(types schema sub "/segment")
+     (->Triple sub "/segment/contains" item-keyword)
+     (types schema item-keyword "/item/poem")
+     (map
+       (fn [i]
+         (let [line (nth lines i)]
+           (->Triple item-keyword "/item/poem/line" ^{:order i} [line])))
+       (range (count lines)))]))
 
 (defn- image-segment
   [sub url alt-text]
@@ -173,10 +174,8 @@
      (->Triple item-keyword "/item/inline/text" (str/join " " lines))]))
 
 (def intro-essay-sequence
-  [(essay-series [:monad :welcome ;:i-am-dan :connections :apologies
-     ])
-   ; (directive-under-construction :connections :apologies)
-  ])
+  [(essay-series [:monad :welcome :i-am-dan :connections :apologies])
+   (directive-under-construction :connections :apologies)])
 
 ; The monad
 ; This essay_segment serves as the default entry point into the graph.
@@ -206,11 +205,9 @@
 (def welcome
   (essay :welcome "Welcome" (fn [t] [
     (initiate t :wave-emoji)
-    ; (->Triple :welcome "/essay/contains" :wave-emoji)
     (big-emoji-segment :wave-emoji "&#x1f44b")
 
     (push-block t :welcome-1)
-    ; (->Triple :wave-emoji "/segment/flow/block" :welcome-1)
     (text-segment :welcome-1
       "Hi there! I'm Daniel Gierl, and I'd like to welcome you to my personal"
       "website, Transcendental Metaphysics! I use this space to explore"
@@ -219,7 +216,6 @@
       "the time you spend here is time well spent.")
 
     (push-block t :welcome-2)
-    ; (->Triple :welcome-1 "/segment/flow/block" :welcome-2)
     (text-segment :welcome-2
       "I apologize in advance for any issues you may encounter with the"
       "unorthodox structure of the site; I've been using it as a playground"
@@ -231,73 +227,87 @@
     ; (text-segment :welcome-2-1
     ;   "and, as a backend engineer, this was a recipe for, ummm, how shall we"
     ;   "say, *curious* frontend design choices.")
-  ])))
+
     ; (footnote :welcome :footnote-1
     ;   "In clojure, no less. I used it as an opportunity to teach myself"
     ;   "the language. There is no learning quite like doing.")
-    ; (text :welcome-3
-    ;   "The whole site is structured as a big, tangled graph. What you're"
-    ;   "reading here is as close to a proper 'beginning' as it gets, and there"
-    ;   "is nothing out there that resembles an 'end'. My intention is that"
-    ;   "wandering through these pages will be an experience not unlike wandering"
-    ;   "through a garden maze; getting lost is half the fun, and there is all"
-    ;   "kinds of"
+
+    (push-block t :welcome-3)
+    (text-segment :welcome-3
+      "The whole site is structured as a big, tangled graph. What you're"
+      "reading here is as close to a proper 'beginning' as it gets, and there"
+      "is nothing out there that resembles an 'end'. My intention is that"
+      "wandering through these pages will be an experience not unlike wandering"
+      "through a garden maze; getting lost is half the fun, and there is all"
+      "kinds of")
     ;   (tangent :footnote-2 "treasure")
     ;   "hidden away for you to find.")
     ; (footnote :welcome :footnote-2
     ;   "As an aside, I have tried to make the URLs somewhat stable so that they"
     ;   "can be shared and saved, but I can only guarantee a modicum of stability"
     ;   "in a shifting sea of ideas.")
-    ; (poem :welcome-4
-    ;   "May you find that which you search for"
-    ;   "May your bridges meet you halfway"
-    ;   "May you never lose yourself in darkness"
-    ;   "And may the light of God shine brightly on your soul")
-    ; (types schema :praying-hands-emoji "/item/big_emoji")
-    ; (->Triple :praying-hands-emoji "/item/big_emoji/emoji" "&#x1f64f")
 
-    ; I Am Dan
-    ; (essay :i-am-dan "I Am Dan" [
-    ;   :yellow-socks :salient-details :prefer-socks :not-privacy])
-    ; (text :yellow-socks
-    ;   "My name's Daniel Gierl. I was a plump, healthy baby with a full head of"
-    ;   "hair, and I went home from the hospital wearing little yellow socks.")
+    (push-block t :welcome-4)
+    (poem-segment :welcome-4
+      "May you find that which you search for"
+      "May your bridges meet you halfway"
+      "May you never lose yourself in darkness"
+      "And may the light of God shine brightly on your soul")
+
+    (push-block t :praying-hands-emoji)
+    (big-emoji-segment :praying-hands-emoji "&#x1f44b")
+  ])))
+
+(def i-am-dan
+  (essay :i-am-dan "I Am Dan" (fn [t] [
+    (initiate t :yellow-socks)
+    (text-segment :yellow-socks
+      "My name's Daniel Gierl. I was a plump, healthy baby with a full head of"
+      "hair, and I went home from the hospital wearing little yellow socks.")
+
     ; ; TODO yellow socks (preferably a photo)
-    ; (text :salient-details
-    ;   "In life, we often overlook the smaller, sentimental details. A person is"
-    ;   "readily reduced to their age and occupation, their parentage, their"
-    ;   "claim to fame... There's nothing wrong with this; it happens for a"
-    ;   "reason. If you're only going to remember one thing about a person, it"
-    ;   "makes sense to remember just their most salient trait. Sometimes, the"
-    ;   "salient trait isn't even what they would think is noteworthy: it's not"
-    ;   "uncommon in the annals of history to find a person whose existence is"
-    ;   "remembered solely because of the consequences of some rash action or"
-    ;   "small act of kindness. The passions, trials, and triumphs that they"
-    ;   "occupied their lives with are forgotten on account of being a bit too"
-    ;   "mundane, a bit too undocumented.")
-    ; (text :prefer-socks
-    ;   "I do have a diary, but it's not on this website. If you read enough of"
-    ;   "this, you will surely get a great sense of who I am, but it's not"
-    ;   "*really* supposed to be about me. Given this opportunity to toot my own"
-    ;   "horn, I'd much rather be known as \"the guy who went home from the"
-    ;   "hospital in yellow socks\" than anything else.")
-    ; (text :not-privacy
-    ;   "It's not even about privacy. I tell a lot of stories, so you will surely"
-    ;   "get plenty of opportunities to get to know me. You'll just have to dig"
-    ;   "around a little to find them.")
 
-    ; Connections
-    ; (essay :connections "Connections" [])
+    (push-block t :salient-details)
+    (text-segment :salient-details
+      "In life, we often overlook the smaller, sentimental details. A person is"
+      "readily reduced to their age and occupation, their parentage, their"
+      "claim to fame... There's nothing wrong with this; it happens for a"
+      "reason. If you're only going to remember one thing about a person, it"
+      "makes sense to remember just their most salient trait. Sometimes, the"
+      "salient trait isn't even what they would think is noteworthy: it's not"
+      "uncommon in the annals of history to find a person whose existence is"
+      "remembered solely because of the consequences of some rash action or"
+      "small act of kindness. The passions, trials, and triumphs that they"
+      "occupied their lives with are forgotten on account of being a bit too"
+      "mundane, a bit too undocumented.")
+
+    (push-block t :prefer-socks)
+    (text-segment :prefer-socks
+      "I do have a diary, but it's not on this website. If you read enough of"
+      "this, you will surely get a great sense of who I am, but it's not"
+      "*really* supposed to be about me. Given this opportunity to toot my own"
+      "horn, I'd much rather be known as \"the guy who went home from the"
+      "hospital in yellow socks\" than anything else.")
+    ])))
+
+(def connections
+  (essay :connections "Connections" (fn [t] [
+    (initiate t :todo-connections)
+    (text-segment :todo-connections "TODO connections")
     ; TODO(gierl) contact information, respective responsibilities
+  ])))
 
-    ; Apologies
-    ; (essay :apologies "Apologies" [])
+(def apologies
+  (essay :apologies "Apologies" (fn [t] [
+    (initiate t :todo-apologies)
+    (text-segment :todo-apologies "TODO apologies")
     ; TODO(gierl) apologize
+  ])))
 
 (def graph
   (construct-graph
     (apply-directives
-      intro-essay-sequence monad welcome)))
+      intro-essay-sequence monad welcome i-am-dan connections apologies)))
 
 (defn -main
   "Validates the website's graph, and generates its files"
