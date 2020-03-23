8f87a20185a1967f938484899bba58f8f5605105
BeABetterHumanBeing <daniel.erik.gierl@gmail.com>
2020-03-22 19:49:03 -0700

    Adding graph overlays to V3

--- src/transcendentalism/graph_v3.clj
+++ src/transcendentalism/graph_v3.clj
@@ -1,4 +1,5 @@
-(ns transcendentalism.graph-v3)
+(ns transcendentalism.graph-v3
+  (:require [clojure.set :as set]))
 
 ; A graph is a map from :sub to Graphlet. v is some value (possibly nil), and
 ; p-os is a map from predicate to set of :objs that have that predicate.
@@ -21,22 +22,33 @@
      if it doesn't already exist"))
 
 (defprotocol GraphV3
+  (get-graphlet [graph sub] "Returns the graphlet for the sub, or nil")
   (get-raw-data [graph] "Returns the raw map underlying the graph")
   (merge-graph [graph other]
     "Returns a new graph created from merging another graph into this one"))
 
 (defn create-graph-v3
   ([] (create-graph-v3 {}))
-  ([raw-data]
+  ([raw-data] (create-graph-v3 raw-data nil))
+  ([raw-data base-graph]
    (reify
      GraphV3
+     (get-graphlet [graph sub]
+       (let [graphlet (raw-data sub)]
+         (if (and (nil? graphlet) (not (nil? base-graph)))
+             (get-graphlet base-graph sub)
+             graphlet)))
      (get-raw-data [graph] raw-data)
      (merge-graph [graph other]
-       (create-graph-v3 (merge raw-data (get-raw-data other))))
+       (create-graph-v3 (merge raw-data (get-raw-data other)) base-graph))
      ReadGraph
-     (read-ss [graph] (into #{} (keys raw-data)))
-     (read-v [graph sub] (:v (raw-data sub)))
-     (read-ps [graph sub] (into #{} (keys (:p-os (raw-data sub) {}))))
+     (read-ss [graph]
+       (let [my-subs (into #{} (keys raw-data))]
+         (if (nil? base-graph)
+             my-subs
+             (set/union my-subs (read-ss base-graph)))))
+     (read-v [graph sub] (:v (get-graphlet graph sub)))
+     (read-ps [graph sub] (into #{} (keys (:p-os (get-graphlet graph sub) {}))))
      (read-os [graph sub pred]
        (if (instance? java.util.regex.Pattern pred)
            (reduce-kv
@@ -44,16 +56,18 @@
                (if (nil? (re-matches pred p))
                    result
                    (into result os)))
-             #{} (:p-os (raw-data sub) {}))
-           ((:p-os (raw-data sub) {}) pred #{})))
+             #{} (:p-os (get-graphlet graph sub) {}))
+           ((:p-os (get-graphlet graph sub) {}) pred #{})))
      WriteGraph
      (write-v [graph sub val]
        (create-graph-v3
-         (assoc raw-data sub (->Graphlet val (:p-os (raw-data sub) {})))))
+         (assoc raw-data sub (->Graphlet val (:p-os (get-graphlet graph sub) {})))
+         base-graph))
      (write-o [graph sub pred obj]
-       (let [p-os (:p-os (raw-data sub) {})]
+       (let [p-os (:p-os (get-graphlet graph sub) {})]
          (create-graph-v3
            (assoc raw-data sub
-             (->Graphlet (:v (raw-data sub))
+             (->Graphlet (:v (get-graphlet graph sub))
                          (assoc p-os pred
-                           (conj (p-os pred #{}) obj))))))))))
+                           (conj (p-os pred #{}) obj))))
+           base-graph))))))
