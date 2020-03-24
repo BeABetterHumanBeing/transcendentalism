1bae8476170b340920271198cedc6ba4334f4d81
BeABetterHumanBeing <daniel.erik.gierl@gmail.com>
2020-03-23 19:00:04 -0700

    Adding Tablet, which can merge access to subjects from disparate GraphV3s, and associate them with metadata. Added graph names; cute feature, useful for debugging

--- src/transcendentalism/graph_v3.clj
+++ src/transcendentalism/graph_v3.clj
@@ -22,17 +22,19 @@
      if it doesn't already exist"))
 
 (defprotocol GraphV3
+  (get-graph-name [graph])
   (get-graphlet [graph sub] "Returns the graphlet for the sub, or nil")
   (get-raw-data [graph] "Returns the raw map underlying the graph")
   (merge-graph [graph other]
     "Returns a new graph created from merging another graph into this one"))
 
 (defn create-graph-v3
-  ([] (create-graph-v3 {}))
-  ([raw-data] (create-graph-v3 raw-data nil))
-  ([raw-data base-graph]
+  ([name] (create-graph-v3 name {}))
+  ([name raw-data] (create-graph-v3 name raw-data nil))
+  ([name raw-data base-graph]
    (reify
      GraphV3
+     (get-graph-name [graph] name)
      (get-graphlet [graph sub]
        (let [graphlet (raw-data sub)]
          (if (and (nil? graphlet) (not (nil? base-graph)))
@@ -40,7 +42,7 @@
              graphlet)))
      (get-raw-data [graph] raw-data)
      (merge-graph [graph other]
-       (create-graph-v3 (merge raw-data (get-raw-data other)) base-graph))
+       (create-graph-v3 name (merge raw-data (get-raw-data other)) base-graph))
      ReadGraph
      (read-ss [graph]
        (let [my-subs (into #{} (keys raw-data))]
@@ -61,13 +63,65 @@
      WriteGraph
      (write-v [graph sub val]
        (create-graph-v3
+         name
          (assoc raw-data sub (->Graphlet val (:p-os (get-graphlet graph sub) {})))
          base-graph))
      (write-o [graph sub pred obj]
        (let [p-os (:p-os (get-graphlet graph sub) {})]
          (create-graph-v3
+           name
            (assoc raw-data sub
              (->Graphlet (:v (get-graphlet graph sub))
                          (assoc p-os pred
                            (conj (p-os pred #{}) obj))))
            base-graph))))))
+
+(defprotocol Tablet
+  (add-entry [tablet sub graph metadata]
+    "Returns a new tablet with an added entry")
+  (update-entry [tablet sub new-sub new-graph new-metadata]
+    "Returns a new tablet with an updated entry")
+  (remove-entry [tablet sub] "Returns a new tablet without the given entry")
+  (merge-tablet [tablet other] [tablet other f]
+    "Returns a new tablet with entries in the other tablet overwriting it. If f
+     is provided, uses it to update merged metadata with (f other metadata).")
+  (get-graph [tablet sub] "Returns the underlying graph the sub is a part of")
+  (get-metadata [tablet sub])
+  (get-bindings [tablet] "Returns the bindings"))
+
+(defn create-read-write-tablet
+  [data bindings]
+  (reify
+    Tablet
+    (add-entry [tablet sub graph metadata]
+      (create-read-write-tablet (assoc data sub [graph metadata]) bindings))
+    (update-entry [tablet sub new-sub new-graph new-metadata]
+      (add-entry
+        (remove-entry tablet sub)
+        new-sub new-graph new-metadata))
+    (remove-entry [tablet sub]
+      (create-read-write-tablet (dissoc data sub) bindings))
+    (merge-tablet [tablet other]
+      (merge-tablet tablet other (fn [_ metadata] metadata)))
+    (merge-tablet [tablet other f]
+      (reduce
+        (fn [result sub]
+          (add-entry result
+            sub (get-graph other sub) (f other (get-metadata other sub))))
+        tablet (read-ss other)))
+    (get-graph [tablet sub] (first (data sub [(create-graph-v3 "temp")])))
+    (get-metadata [tablet sub] (second (data sub [nil {}])))
+    (get-bindings [tablet] bindings)
+    ReadGraph
+    (read-ss [tablet] (into #{} (keys data)))
+    (read-v [tablet sub] (read-v (get-graph tablet sub) sub))
+    (read-ps [tablet sub] (read-ps (get-graph tablet sub) sub))
+    (read-os [tablet sub pred] (read-os (get-graph tablet sub) sub pred))
+    WriteGraph
+    (write-v [tablet sub val]
+      (add-entry tablet
+        sub (write-v (get-graph tablet sub) sub val) (get-metadata tablet sub)))
+    (write-o [tablet sub pred obj]
+      (add-entry tablet sub
+                        (write-o (get-graph tablet sub) sub pred obj)
+                        (get-metadata tablet sub)))))
--- test/transcendentalism/graph_v3_test.clj
+++ test/transcendentalism/graph_v3_test.clj
@@ -3,13 +3,14 @@
             [transcendentalism.graph-v3 :refer :all]))
 
 (deftest graph-read-write-test
-  (let [graph (-> (create-graph-v3)
+  (let [graph (-> (create-graph-v3 "test")
                   (write-v :a 7)
                   (write-v :b 8)
                   (write-o :a "/foo" "lol")
                   (write-o :a "/foo" "kek")
                   (write-o :a "/bar" 21))]
     (testing "Test reading and writing to the graph"
+      (is (= "test" (get-graph-name graph)))
       (is (= #{:a :b} (read-ss graph)))
       (is (= 7 (read-v graph :a)))
       (is (= 8 (read-v graph :b)))
@@ -25,18 +26,19 @@
       (is (empty? (read-os graph :a #"/\d*"))))))
 
 (deftest graph-overlay-test
-  (let [base-graph (-> (create-graph-v3)
+  (let [base-graph (-> (create-graph-v3 "test-base")
                        (write-v :a 7)
                        (write-v :b 8)
                        (write-v :c 9)
                        (write-o :a "/foo" "lol")
                        (write-o :a "/bar" "kek")),
-        graph (-> (create-graph-v3 {} base-graph)
+        graph (-> (create-graph-v3 "test-overlay" {} base-graph)
                   (write-v :a 10)
                   (write-o :a "/bar" "eke")
                   (write-o :b "/foo" "nom")
                   (write-o :d "/baz" "hah"))]
     (testing "Test reading and writing to an overlay graph"
+      (is (= "test-overlay" (get-graph-name graph)))
       (is (= #{:a :b :c :d} (read-ss graph)))
       (is (= 10 (read-v graph :a)))
       (is (= 8 (read-v graph :b)))
@@ -50,3 +52,64 @@
       (is (= #{"kek" "eke"} (read-os graph :a "/bar")))
       (is (= #{"nom"} (read-os graph :b "/foo")))
       (is (= #{"hah"} (read-os graph :d "/baz"))))))
+
+(deftest tablet-test
+  (let [graph-a (-> (create-graph-v3 "graph-a")
+                    (write-v :a1 7)
+                    (write-v :a2 8)
+                    (write-o :a1 "/foo" "blah")
+                    (write-o :a2 "/foo" "yack")),
+        graph-b (-> (create-graph-v3 "graph-b")
+                    (write-v :b1 1)
+                    (write-v :b2 2)
+                    (write-o :b1 "/bar" "blue")
+                    (write-o :b2 "/bar" "yellow")),
+        tablet-a (-> (create-read-write-tablet {} {:c 100, :d 200})
+                     (add-entry :a1 graph-a {:my-meta "cat"})
+                     (add-entry :a2 graph-a {:my-meta "dog"})
+                     (remove-entry :a1)
+                     (write-v :a1 701)
+                     (write-v :a2 801)),
+        tablet-b (-> (create-read-write-tablet {} {:e 300})
+                     (add-entry :b1 graph-b {:my-meta "mouse"})
+                     (add-entry :b2 graph-b {:my-meta "rat"})
+                     (update-entry :b1 :b3 graph-b {:my-meta "cow"})
+                     (write-o :b1 "/baz" "rock")
+                     (write-o :b2 "/baz" "paper")
+                     (write-o :b3 "/baz" "scissors")),
+        tablet-ab (-> (merge-tablet tablet-a tablet-b
+                                    (fn [_ metadata]
+                                      (assoc metadata :merge-info true)))
+                      (write-v :b1 101)
+                      (write-o :a1 "/snu" "hello")
+                      (write-o :b2 "/snu" "hola"))]
+    (testing "Test reading and writing to a tablet"
+      (is (= (get-bindings tablet-a) (get-bindings tablet-ab)))
+      (is (= #{:a1 :a2 :b1 :b2 :b3} (read-ss tablet-ab)))
+      (is (empty? (get-metadata tablet-ab :a1)))
+      (is (= {:my-meta "dog"} (get-metadata tablet-ab :a2)))
+      (is (= {:merge-info true} (get-metadata tablet-ab :b1)))
+      (is (= {:merge-info true :my-meta "rat"} (get-metadata tablet-ab :b2)))
+      (is (= {:merge-info true :my-meta "cow"} (get-metadata tablet-ab :b3)))
+      (is (= "temp" (get-graph-name (get-graph tablet-ab :a1))))
+      (is (= "graph-a" (get-graph-name (get-graph tablet-ab :a2))))
+      (is (= "temp" (get-graph-name (get-graph tablet-ab :b1))))
+      (is (= "graph-b" (get-graph-name (get-graph tablet-ab :b2))))
+      (is (= "graph-b" (get-graph-name (get-graph tablet-ab :b3))))
+      (is (= 701 (read-v tablet-ab :a1)))
+      (is (= 801 (read-v tablet-ab :a2)))
+      (is (= 101 (read-v tablet-ab :b1)))
+      (is (= 2 (read-v tablet-ab :b2)))
+      (is (nil? (read-v tablet-ab :b3)))
+      (is (= #{"/snu"} (read-ps tablet-ab :a1)))
+      (is (= #{"/foo"} (read-ps tablet-ab :a2)))
+      (is (= #{"/baz"} (read-ps tablet-ab :b1)))
+      (is (= #{"/bar" "/baz" "/snu"} (read-ps tablet-ab :b2)))
+      (is (= #{"/baz"} (read-ps tablet-ab :b3)))
+      (is (= #{"hello"} (read-os tablet-ab :a1 "/snu")))
+      (is (= #{"yack"} (read-os tablet-ab :a2 "/foo")))
+      (is (= #{"rock"} (read-os tablet-ab :b1 "/baz")))
+      (is (= #{"yellow"} (read-os tablet-ab :b2 "/bar")))
+      (is (= #{"paper"} (read-os tablet-ab :b2 "/baz")))
+      (is (= #{"hola"} (read-os tablet-ab :b2 "/snu")))
+      (is (= #{"scissors"} (read-os tablet-ab :b3 "/baz"))))))
