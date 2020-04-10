656159be7ad513a3cb15e6c363b03784547f7547
BeABetterHumanBeing <daniel.erik.gierl@gmail.com>
2020-04-10 10:54:01 -0700

    Finished translating V2 to V3 schema. Still need to do graph conversion, and validate that current graph conforms to V3 schema

--- src/transcendentalism/schema_v3.clj
+++ src/transcendentalism/schema_v3.clj
@@ -246,185 +246,187 @@
       },
     }))
 
-; (def big-emoji-schema
-;   (schematize-type "/item/big_emoji"
-;     {
-;       :description "A series of big emoji",
-;       :super-type "/type/item",
-;     }
-;     {
-;       "/emoji" {
-;         :description "The sequence of emoji to render",
-;         :range-type :string,
-;         :required true,
-;         :unique true,
-;       },
-;     }))
+(defn big-emoji-type
+  [graph]
+  (build-type-graph graph :big-emoji-type #{:item-type}
+    {
+      :description "A series of big emoji",
+      :preds {
+        "/emoji" {
+          :description "The sequence of emoji to render",
+          :range-type :string,
+          :required true,
+          :unique true,
+        },
+      },
+    }))
 
-; (def q-and-a-schema
-;   (schematize-type "/item/q_and_a"
-;     {
-;       :description "A question and answer",
-;       :super-type "/type/item",
-;     }
-;     {
-;       "/question" {
-;         :description "The question being asked",
-;         :range-type "/type/segment",
-;         :required true,
-;         :unique true,
-;       },
-;       "/answer" {
-;         :description "The answer being given",
-;         :range-type "/type/segment",
-;         :required true,
-;         :unique true,
-;       },
-;     }))
+(defn q-and-a-type
+  [graph]
+  (build-type-graph graph :q-and-a-type #{:item-type}
+    {
+      :description "A question and answer",
+      :preds {
+        "/question" {
+          :description "The question being asked",
+          :range-type "/type/segment",
+          :required true,
+          :unique true,
+        },
+        "/answer" {
+          :description "The answer being given",
+          :range-type "/type/segment",
+          :required true,
+          :unique true,
+        },
+      },
+    }))
 
-; (def bullet-list-schema
-;   (schematize-type "/item/bullet_list"
-;     {
-;       :description "A list with bullet points",
-;       :super-type "/type/item",
-;     }
-;     {
-;       "/header" {
-;         :description "What appears above the list to introduce it",
-;         :range-type "/type/segment",
-;         :unique true,
-;       }
-;       "/point" (with-order "/item/bullet_list/point" {
-;         :description "A bullet-pointed item. Uses :order property to sort",
-;         :range-type "/type/segment",
-;         :required true,
-;       }),
-;       "/is_ordered" {
-;         :description "Whether the list should be ordered, or bullet-pointed",
-;         :range-type :bool,
-;         :unique true,
-;         :required true,
-;       },
-;     }))
+(defn bullet-list-type
+  [graph]
+  (build-type-graph graph :bullet-list-type #{:item-type}
+    {
+      :description "A list with bullet points",
+      :preds {
+        "/header" {
+          :description "What appears above the list to introduce it",
+          :range-type "/type/segment",
+          :unique true,
+        }
+        "/point" {
+          :description "A bullet-pointed item. Uses :order property to sort",
+          :range-type "/type/segment",
+          :required true,
+          :ordered true,
+        },
+        "/is_ordered" {
+          :description "Whether the list should be ordered, or bullet-pointed",
+          :range-type :bool,
+          :unique true,
+          :required true,
+        },
+      },
+    }))
 
-; (def contact-schema
-;   (schematize-type "/item/contact"
-;     {
-;       :description "Provides contact information",
-;       :super-type "/type/item",
-;     }
-;     {
-;       "/email" {
-;         :description "An email address that can be mailed to",
-;         :range-type :string,
-;         :unique true,
-;         :required true,
-;       },
-;     }))
+(defn contact-type
+  [graph]
+  (build-type-graph graph :contact-type #{:item-type}
+    {
+      :description "Provides contact information",
+      :preds {
+        "/email" {
+          :description "An email address that can be mailed to",
+          :range-type :string,
+          :unique true,
+          :required true,
+        },
+      },
+    }))
 
-; (def definition-schema
-;   (schematize-type "/item/definition"
-;     {
-;       :description "Provides a definition for a word",
-;       :super-type "/type/item",
-;     }
-;     {
-;       "/word" {
-;         :description "The word",
-;         :range-type :string,
-;         :unique true,
-;         :required true,
-;       },
-;       "/part_of_speech" {
-;         :description "The part of speech the word belongs to",
-;         :range-type [:noun :adjective],
-;         :unique true,
-;         :required true,
-;       },
-;       "/definition" (with-order "/item/definition/definition" {
-;         :description "A definition of the word",
-;         :range-type :string,
-;         :required true,
-;       }),
-;     }))
+(defn definition-type
+  [graph]
+  (build-type-graph graph :definition-type #{:item-type}
+    {
+      :description "Provides a definition for a word",
+      :preds {
+        "/word" {
+          :description "The word",
+          :range-type :string,
+          :unique true,
+          :required true,
+        },
+        "/part_of_speech" {
+          :description "The part of speech the word belongs to",
+          :range-type #{:noun :adjective},
+          :unique true,
+          :required true,
+        },
+        "/definition" {
+          :description "A definition of the word",
+          :range-type :string,
+          :required true,
+          :ordered true,
+        },
+      },
+    }))
 
-; (def table-schema
-;   (schematize-type "/item/table"
-;     {
-;       :description "A 2D table of items",
-;       :super-type "/type/item",
-;     }
-;     {
-;       "/cell" {
-;         :description "A cell in the table",
-;         :range-type "/type/segment",
-;         :required true,
-;         :properties {
-;           "/column" {
-;             :description "The column the cell appears in",
-;             :range-type :number,
-;             :required true,
-;           },
-;           "/row" {
-;             :description "The row the cell appears in",
-;             :range-type :number,
-;             :required true,
-;           },
-;         }
-;       },
-;       "/label" {
-;         :description "A label for a column or row",
-;         :range-type "/type/segment"
-;         :properties {
-;           "/column" {
-;             :description "The column the label applies to",
-;             :range-type :number,
-;             :exclusive #{"/row"},
-;           },
-;           "/row" {
-;             :description "The row the label applies to",
-;             :range-type :number,
-;             :exclusive #{"/column"},
-;           },
-;         },
-;       },
-;     }))
+(defn table-type
+  [graph]
+  (build-type-graph graph :table-type #{:item-type}
+    {
+      :description "A 2D table of items",
+      :preds {
+        "/cell" {
+          :description "A cell in the table",
+          :range-type "/type/segment",
+          :required true,
+          :properties {
+            "/column" {
+              :description "The column the cell appears in",
+              :range-type :number,
+              :required true,
+            },
+            "/row" {
+              :description "The row the cell appears in",
+              :range-type :number,
+              :required true,
+            },
+          }
+        },
+        "/label" {
+          :description "A label for a column or row",
+          :range-type "/type/segment",
+          :mutually-exclusive #{"/row" "/column"},
+          :properties {
+            "/column" {
+              :description "The column the label applies to",
+              :range-type :number,
+            },
+            "/row" {
+              :description "The row the label applies to",
+              :range-type :number,
+            },
+          },
+        },
+      },
+    }))
 
-; (def raw-html-schema
-;   (schematize-type "/item/raw_html"
-;     {
-;       :description "Raw HTML, bypassing the schema. Use with caution.",
-;       :super-type "/type/item",
-;     }
-;     {
-;       "/contains" {
-;         :description "The HTML content as a string",
-;         :range-type :string,
-;         :unique true,
-;         :required true,
-;       },
-;     }))
+(defn raw-html-type
+  [graph]
+  (build-type-graph graph :raw-html-type #{:item-type}
+    {
+      :description "Raw HTML, bypassing the schema. Use with caution.",
+      :preds {
+        "/contains" {
+          :description "The HTML content as a string",
+          :range-type :string,
+          :unique true,
+          :required true,
+        },
+      },
+    }))
 
-; (def thesis-schema
-;   (schematize-type "/item/thesis"
-;     {
-;       :description "A thesis, key point, or other information to call out",
-;       :super-type "/type/item",
-;     }
-;     {
-;       "/contains" {
-;         :description "The thesis",
-;         :range-type :string,
-;         :unique true,
-;         :required true,
-;       },
-;     }))
+(defn thesis-type
+  [graph]
+  (build-type-graph graph :thesis-type #{:item-type}
+    {
+      :description "A thesis, key point, or other information to call out",
+      :preds {
+        "/contains" {
+          :description "The thesis",
+          :range-type :string,
+          :unique true,
+          :required true,
+        },
+      },
+    }))
 
 (def type-graph
   (reduce #(merge-graph %1 (%2 %1))
     (create-graph-v3)
     [event-type essay-type segment-type item-type inline-item-type image-type
-     quote-type poem-type]))
+     quote-type poem-type big-emoji-type q-and-a-type bullet-list-type
+     definition-type table-type raw-html-type thesis-type]))
 
 (defn validate-graph-v3
   [graph]
