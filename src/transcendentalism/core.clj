be5f5ef2dd151fa61407b3c7fca67339eb40cdaa
BeABetterHumanBeing <daniel.erik.gierl@gmail.com>
2020-04-10 18:22:32 -0700

    Removed aborted V2

--- src/transcendentalism/core.clj
+++ src/transcendentalism/core.clj
@@ -3,8 +3,7 @@
             [transcendentalism.toolbox :refer :all]))
 
 ; TODO - change use to :require
-(use 'transcendentalism.constraint-v2
-     'transcendentalism.directive
+(use 'transcendentalism.directive
      'transcendentalism.essay
      'transcendentalism.essays.consciousness
      'transcendentalism.essays.epistemology
@@ -21,7 +20,6 @@
      'transcendentalism.glossary
      'transcendentalism.graph
      'transcendentalism.schema
-     'transcendentalism.schema-v2
      'transcendentalism.schema-v3)
 
 (def meta-directives
@@ -43,15 +41,10 @@
   [& args]
   (apply set-flags args)
   (let [graph-v1 (time-msg "Construct Graph" (collect-essays)),
-        ; Translation must be done into v2 for apply directives.
-        graph-v2 (time-msg "V1->V2" (graph-to-v2 graph-v1)),
         graph-v3 (time-msg "V1->V3" (graph-to-v3 graph-v1)),
-        graph-final-v2 (time-msg "DirectV2" (direct-graph schema graph-v2)),
-        ; But back to v1 for sxs validation comparison and rendering.
-        [v3-errors graph-final-v3] (time-msg "ValidateV3" (validate-graph-v3 graph-v3))
+        [v3-errors graph-final-v3] (time-msg "ValidateV3" (validate-graph-v3 graph-v3)),
         graph-final-v1 (time-msg "V3->V1" (graph-to-v1 graph-final-v3))]
     (if (and (empty? v3-errors)
-             (time-msg "ValidateV2" (validate-graph-v2 schema graph-final-v2))
              (time-msg "ValidateV1" (validate-graph-v1 schema-v1 graph-final-v1)))
       (if (flag :enable-v2)
         (println "Skipping graph generation")
