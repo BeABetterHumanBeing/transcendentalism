362deb38a9737c9e0c0365f80a94e215fe7328f4
BeABetterHumanBeing <daniel.erik.gierl@gmail.com>
2020-04-27 15:55:40 -0700

    Adding a big emoji renderer

--- src/transcendentalism/components/big_emoji.clj
+++ src/transcendentalism/components/big_emoji.clj
@@ -1,5 +1,9 @@
 (ns transcendentalism.components.big-emoji
-  (:require [transcendentalism.constraint :refer :all]))
+  (:require [transcendentalism.constraint :refer :all]
+            [transcendentalism.css :refer :all]
+            [transcendentalism.graph-v3 :refer :all]
+            [transcendentalism.html :refer :all]
+            [transcendentalism.render :refer :all]))
 
 (defn big-emoji-component
   [graph]
@@ -14,4 +18,15 @@
           :unique true,
         },
       },
-    }))
+    }
+    (reify Renderer
+      (get-renderer-name [renderer] "big-emoji")
+      (get-priority [renderer] 10)
+      (render-html [renderer params graph sub]
+        (div {"class" "emoji"}
+          (unique-or-nil graph sub "/item/big_emoji/emoji")))
+      (render-css [renderer]
+        (css "div" {"class" "emoji"}
+          (font-size "100px")
+          (text-align "center")))
+      (render-js [renderer] ""))))
