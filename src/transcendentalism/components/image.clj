2e12f041e865abfde884f1176a083c219fc1510e
BeABetterHumanBeing <daniel.erik.gierl@gmail.com>
2020-04-28 14:16:16 -0700

    Adding a renderer for the image component

--- src/transcendentalism/components/image.clj
+++ src/transcendentalism/components/image.clj
@@ -1,5 +1,9 @@
 (ns transcendentalism.components.image
-  (:require [transcendentalism.constraint :refer :all]))
+  (:require [transcendentalism.constraint :refer :all]
+            [transcendentalism.css :refer :all]
+            [transcendentalism.graph-v3 :refer :all]
+            [transcendentalism.html :refer :all]
+            [transcendentalism.render :refer :all]))
 
 (defn image-component
   [graph]
@@ -30,4 +34,26 @@
           :unique true,
         },
       },
-    }))
+    }
+    (reify Renderer
+      (get-renderer-name [renderer] "image")
+      (get-priority [renderer] 10)
+      (render-html [renderer params graph sub]
+        (let [image-url-triple (unique-or-nil graph sub "/item/image/url"),
+              image-alt-text-triple (unique-or-nil graph sub "/item/image/alt_text"),
+              image-width (unique-or-nil graph sub "/item/image/width"),
+              image-height (unique-or-nil graph sub "/item/image/height")]
+          (img {"src" image-url-triple,
+                "alt" image-alt-text-triple,
+                "style" (str (if (nil? image-width)
+                                 ""
+                                 (str "width:" image-width "px"))
+                             ";"
+                             (if (nil? image-height)
+                                 ""
+                                 (str "height:" image-height "px")))})))
+      (render-css [renderer]
+        (css "img" {}
+          (margin "0" "auto")
+          (display "block")))
+      (render-js [renderer] ""))))
