c5d5b186f9bbfc636e932ad2008efa3e0db005d5
BeABetterHumanBeing <daniel.erik.gierl@gmail.com>
2020-04-28 14:34:07 -0700

    Adding renderer for thesis component

--- src/transcendentalism/components/thesis.clj
+++ src/transcendentalism/components/thesis.clj
@@ -1,5 +1,10 @@
 (ns transcendentalism.components.thesis
-  (:require [transcendentalism.constraint :refer :all]))
+  (:require [transcendentalism.color :refer :all]
+            [transcendentalism.constraint :refer :all]
+            [transcendentalism.css :refer :all]
+            [transcendentalism.graph-v3 :refer :all]
+            [transcendentalism.html :refer :all]
+            [transcendentalism.render :refer :all]))
 
 (defn thesis-component
   [graph]
@@ -14,4 +19,19 @@
           :required true,
         },
       },
-    }))
+    }
+    (reify Renderer
+      (get-renderer-name [renderer] "thesis")
+      (get-priority [renderer] 10)
+      (render-html [renderer params graph sub]
+        (div {"class" "thesis"}
+          (unique-or-nil graph sub "/item/thesis/contains")))
+      (render-css [renderer]
+        (css "div" {"class" "thesis"}
+          (border-style "solid")
+          (border-width "2px")
+          (border-color (to-css-color gray))
+          (margin "0px" "75px")
+          (padding "25px")
+          (text-align "center")))
+      (render-js [renderer] ""))))
