49fcc2524b3ae2b08a136e87e351fa248bf759a6
BeABetterHumanBeing <daniel.erik.gierl@gmail.com>
2020-04-28 14:20:35 -0700

    Adding a renderer for the quote component

--- src/transcendentalism/components/quote.clj
+++ src/transcendentalism/components/quote.clj
@@ -1,5 +1,10 @@
 (ns transcendentalism.components.quote
-  (:require [transcendentalism.constraint :refer :all]))
+  (:require [clojure.string :as str]
+            [transcendentalism.constraint :refer :all]
+            [transcendentalism.css :refer :all]
+            [transcendentalism.graph-v3 :refer :all]
+            [transcendentalism.html :refer :all]
+            [transcendentalism.render :refer :all]))
 
 (defn quote-component
   [graph]
@@ -19,4 +24,24 @@
           :unique true,
         },
       },
-    }))
+    }
+    (reify Renderer
+      (get-renderer-name [renderer] "quote")
+      (get-priority [renderer] 10)
+      (render-html [renderer params graph sub]
+        (let [quote-text (unique-or-nil graph sub "/item/quote/text"),
+              author-or-nil (unique-or-nil graph sub "/item/quote/author"),
+              author (if (nil? author-or-nil) "Anonymous" author-or-nil)]
+          (div {"class" "quote"}
+            (p {} (str "\"" quote-text "\""))
+            (p {"class" "author"} (str "-" author)))))
+      (render-css [renderer]
+        (str/join "\n" [
+          (css "div" {"class" "quote"}
+            (padding "0px" "75px")
+            (font-size "large")
+            (font-family "Times" "serif")
+            (font-style "italic"))
+          (css "p" {"class" "author"}
+            (text-align "right"))]))
+      (render-js [renderer] ""))))
