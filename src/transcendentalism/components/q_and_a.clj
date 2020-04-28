5a137a7540ce5e5cf5fe8e14c4cec8b8bb02658b
BeABetterHumanBeing <daniel.erik.gierl@gmail.com>
2020-04-28 14:42:22 -0700

    Adding renderer to q-and-a component

--- src/transcendentalism/components/q_and_a.clj
+++ src/transcendentalism/components/q_and_a.clj
@@ -1,5 +1,9 @@
 (ns transcendentalism.components.q-and-a
-  (:require [transcendentalism.constraint :refer :all]))
+  (:require [transcendentalism.constraint :refer :all]
+            [transcendentalism.css :refer :all]
+            [transcendentalism.graph-v3 :refer :all]
+            [transcendentalism.html :refer :all]
+            [transcendentalism.render :refer :all]))
 
 (defn q-and-a-component
   [graph]
@@ -20,4 +24,23 @@
           :unique true,
         },
       },
-    }))
+    }
+    (reify Renderer
+      (get-renderer-name [renderer] "q-and-a")
+      (get-priority [renderer] 10)
+      (render-html [renderer params graph sub]
+        (let [q-block (unique-or-nil graph sub "/item/q_and_a/question"),
+              a-block (unique-or-nil graph sub "/item/q_and_a/answer")]
+          (div {"class" "q_and_a"}
+            (div {"class" "q_and_a_header"} "Q:")
+            (str "<i>" (param-aware-render-sub params graph q-block) "</i>")
+            (div {"class" "q_and_a_header"} "A:")
+            (div {} (param-aware-render-sub params graph a-block)))))
+      (render-css [renderer]
+        (css "div" {"class" "q_and_a"}
+          (display "grid")
+          (grid-template-columns "30px" "[qa_separator]" "auto")
+          (grid-row-gap "8px"))
+        (css "div" {"class" "q_and_a_header"}
+          (font-weight "bold")))
+      (render-js [renderer] ""))))
