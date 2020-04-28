09ff0792e2d7955a8e33993453d7f58e09398895
BeABetterHumanBeing <daniel.erik.gierl@gmail.com>
2020-04-28 14:29:59 -0700

    Adding renderer for definition component

--- src/transcendentalism/components/definition.clj
+++ src/transcendentalism/components/definition.clj
@@ -1,5 +1,11 @@
 (ns transcendentalism.components.definition
-  (:require [transcendentalism.constraint :refer :all]))
+  (:require [clojure.string :as str]
+            [transcendentalism.color :refer :all]
+            [transcendentalism.constraint :refer :all]
+            [transcendentalism.css :refer :all]
+            [transcendentalism.graph-v3 :refer :all]
+            [transcendentalism.html :refer :all]
+            [transcendentalism.render :refer :all]))
 
 (defn definition-component
   [graph]
@@ -26,4 +32,28 @@
           :ordered true,
         },
       },
-    }))
+    }
+    (reify Renderer
+      (get-renderer-name [renderer] "definition")
+      (get-priority [renderer] 10)
+      (render-html [renderer params graph sub]
+        (let [word (unique-or-nil graph sub "/item/definition/word"),
+              part-of-speech ({
+                :noun "noun",
+                :adjective "adj.",
+                } (unique-or-nil graph sub "/item/definition/part_of_speech")
+                "unk"),
+              definitions (get-ordered-objs graph sub "/item/definition/definition")]
+          (div {"class" "definition"}
+            (span {} (str "<b>Definition</b> " word " ")
+            (span {} (str "<i>(" part-of-speech ")</i>:"))
+            (apply ul {"class" "bullet_list"}
+              (str/join "\n"
+                (map #(li {} (read-v graph %)) definitions)))))))
+      (render-css [renderer]
+        (css "div" {"class" "definition"}
+          (border-color (to-css-color red))
+          (border-width "1px")
+          (border-style "solid")
+          (padding "10px" "10px" "5px" "10px")))
+      (render-js [renderer] ""))))
