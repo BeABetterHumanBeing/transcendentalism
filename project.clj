0a05022685018a28f2b366dab6e5149c824a97ab
BeABetterHumanBeing <daniel.erik.gierl@gmail.com>
2020-06-22 11:03:33 -0700

    Alphabetized project-level dependency list

--- project.clj
+++ project.clj
@@ -3,18 +3,17 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  ; TODO - alphabetize dependencies
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojure/math.numeric-tower "0.0.4"]
-                 [clojure.java-time "0.3.2"]
-                 [compojure "1.6.1"]
-                 [ring/ring-core "1.8.0"]
-                 [ring/ring-jetty-adapter "1.8.0"]
-                 [amazonica "0.3.152" :exclusions [com.amazonaws/aws-java-sdk
+  :dependencies [[amazonica "0.3.152" :exclusions [com.amazonaws/aws-java-sdk
                                                    com.amazonaws/amazon-kinesis-client
                                                    com.fasterxml.jackson.core/jackson-databind]]
+                 [clojure.java-time "0.3.2"]
                  [com.amazonaws/aws-java-sdk-core "1.11.775"]
-                 [com.amazonaws/aws-java-sdk-s3 "1.11.775"]]
+                 [com.amazonaws/aws-java-sdk-s3 "1.11.775"]
+                 [compojure "1.6.1"]
+                 [org.clojure/clojure "1.10.0"]
+                 [org.clojure/math.numeric-tower "0.0.4"]
+                 [ring/ring-core "1.8.0"]
+                 [ring/ring-jetty-adapter "1.8.0"]]
   :main transcendentalism.core
   :aot [transcendentalism.core]
   ; Development flags are set here. AWS flags are set in flags.clj
